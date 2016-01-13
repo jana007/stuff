9c96aaf44e22253f2ebdee75cc4af92d7340ebf5
Jana <toots1701@gmail.com>
2016-01-13 12:01:32 -0500

    Create liberalStudies.js

---
+++ liberalStudies.js
@@ -0,0 +1,158 @@
+//This is a mess and uncessary
+//Use as example of what not to do
+
+// toggle javascript -->
+// Computer Competency -->
+$(document).ready(function(){$("#comp").click(function(){$("#togcomp").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#IFS3094").click(function(){$("#togIFS3094").toggle();});});
+
+// Diversity in Western Experience -->
+$(document).ready(function(){$("#divwest").click(function(){$("#togdivwest").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#HIS3491").click(function(){$("#togHIS3491").toggle();});});
+$(document).ready(function(){$("#THE2000").click(function(){$("#togTHE2000").toggle();});});
+$(document).ready(function(){$("#MUH2019").click(function(){$("#togMUH2019").toggle();});});
+$(document).ready(function(){$("#SYG2010").click(function(){$("#togSYG2010").toggle();});});
+$(document).ready(function(){$("#PHI2635").click(function(){$("#togPHI2635").toggle();});});
+$(document).ready(function(){$("#HUM3321").click(function(){$("#togHUM3321").toggle();});});
+$(document).ready(function(){$("#IFS2072").click(function(){$("#togIFS2072").toggle();});});
+$(document).ready(function(){$("#SYO3200").click(function(){$("#togSYO3200").toggle();});});
+$(document).ready(function(){$("#SYD3800").click(function(){$("#togSYD3800").toggle();});});
+$(document).ready(function(){$("#NSP3185").click(function(){$("#togNSP3185").toggle();});});
+
+// Cross Cultural Studies -->
+$(document).ready(function(){$("#crosscultural").click(function(){$("#togcrosscultural").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#REL1300").click(function(){$("#togREL1300").toggle();});});
+$(document).ready(function(){$("#REL2315").click(function(){$("#togREL2315").toggle();});});
+$(document).ready(function(){$("#GEA1000").click(function(){$("#togGEA1000").toggle();});});
+$(document).ready(function(){$("#HFT2061").click(function(){$("#togHFT2061").toggle();});});
+$(document).ready(function(){$("#HFT2062").click(function(){$("#togHFT2062").toggle();});});
+$(document).ready(function(){$("#PHM2300").click(function(){$("#togPHM2300").toggle();});});
+$(document).ready(function(){$("#GEO1400").click(function(){$("#togGEO1400").toggle();});});
+$(document).ready(function(){$("#EUH3205").click(function(){$("#togEUH3205").toggle();});});
+$(document).ready(function(){$("#HFT2060").click(function(){$("#togHFT2060").toggle();});});
+$(document).ready(function(){$("#IFS2073").click(function(){$("#togIFS2073").toggle();});});
+$(document).ready(function(){$("#MUH2051").click(function(){$("#togMUH2051").toggle();});});
+$(document).ready(function(){$("#HFT2063").click(function(){$("#togHFT2063").toggle();});});
+$(document).ready(function(){$("#IFS3094two").click(function(){$("#togIFS3094two").toggle();});});
+$(document).ready(function(){$("#CLT3378").click(function(){$("#togCLT3378").toggle();});});
+
+// Humanities and Cultural Practice -->
+$(document).ready(function(){$("#cultural").click(function(){$("#togcultural").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#ART2003C").click(function(){$("#togART2003C").toggle();});});
+$(document).ready(function(){$("#CLT2049").click(function(){$("#togCLT2049").toggle();});});
+$(document).ready(function(){$("#REL1300two").click(function(){$("#togREL1300two").toggle();});});
+$(document).ready(function(){$("#REL2315two").click(function(){$("#togREL2315two").toggle();});});
+$(document).ready(function(){$("#THE2000two").click(function(){$("#togTHE2000two").toggle();});});
+$(document).ready(function(){$("#HUM2210").click(function(){$("#togHUM2210").toggle();});});
+$(document).ready(function(){$("#HUM2235").click(function(){$("#togHUM2235").toggle();});});
+$(document).ready(function(){$("#HUM2250").click(function(){$("#togHUM2250").toggle();});});
+$(document).ready(function(){$("#MUH2019two").click(function(){$("#togMUH2019two").toggle();});});
+$(document).ready(function(){$("#ARH2000").click(function(){$("#togARH2000").toggle();});});
+$(document).ready(function(){$("#HUM3321two").click(function(){$("#togHUM3321two").toggle();});});
+$(document).ready(function(){$("#IFS2073two").click(function(){$("#togIFS2073two").toggle();});});
+$(document).ready(function(){$("#IFS2072two").click(function(){$("#togIFS2072two").toggle();});});
+$(document).ready(function(){$("#MUH2051two").click(function(){$("#togMUH2051two").toggle();});});
+$(document).ready(function(){$("#MUL2010").click(function(){$("#togMUL2010").toggle();});});
+$(document).ready(function(){$("#LIT3438").click(function(){$("#togLIT3438").toggle();});});
+$(document).ready(function(){$("#CLT3378two").click(function(){$("#togCLT3378two").toggle();});});
+
+// E-Series -->
+$(document).ready(function(){$("#eseries").click(function(){$("#togeseries").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#IFS2073three").click(function(){$("#togIFS2073three").toggle();});});
+$(document).ready(function(){$("#IFS2072three").click(function(){$("#togIFS2072three").toggle();});});
+$(document).ready(function(){$("#IFS3094three").click(function(){$("#togIFS3094three").toggle();});});
+$(document).ready(function(){$("#IFS2105").click(function(){$("#togIFS2105").toggle();});});
+
+// Ethics and Social Responsibility -->
+$(document).ready(function(){$("#ethics").click(function(){$("#togethics").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#PHI2635two").click(function(){$("#togPHI2635two").toggle();});});
+$(document).ready(function(){$("#PHM2300two").click(function(){$("#togPHM2300two").toggle();});});
+$(document).ready(function(){$("#PAD3003").click(function(){$("#togPAD3003").toggle();});});
+$(document).ready(function(){$("#LIT3438two").click(function(){$("#togLIT3438two").toggle();});});
+
+
+// History -->
+$(document).ready(function(){$("#history").click(function(){$("#toghistory").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#CLT2049two").click(function(){$("#togCLT2049two").toggle();});});
+$(document).ready(function(){$("#HIS3491two").click(function(){$("#togHIS3491two").toggle();});});
+$(document).ready(function(){$("#EUH3205two").click(function(){$("#togEUH3205two").toggle();});});
+$(document).ready(function(){$("#AMH2020").click(function(){$("#togAMH2020").toggle();});});
+
+// Natural Sciences -->
+$(document).ready(function(){$("#natsci").click(function(){$("#tognatsci").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#HUN1201").click(function(){$("#togHUN1201").toggle();});});
+$(document).ready(function(){$("#CHM1020C").click(function(){$("#togCHM1020C").toggle();});});
+$(document).ready(function(){$("#OCE1001").click(function(){$("#togOCE1001").toggle();});});
+$(document).ready(function(){$("#MET1010").click(function(){$("#togMET1010").toggle();});});
+$(document).ready(function(){$("#EVR1001").click(function(){$("#togEVR1001").toggle();});});
+$(document).ready(function(){$("#EVR1001L").click(function(){$("#togEVR1001L").toggle();});});
+$(document).ready(function(){$("#IFS2105two").click(function(){$("#togIFS2105two").toggle();});});
+
+// Quantitative and Logical Thinking -->
+$(document).ready(function(){$("#quant").click(function(){$("#togquant").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#STA1013").click(function(){$("#togSTA1013").toggle();});});
+$(document).ready(function(){$("#MAC2311").click(function(){$("#togMAC2311").toggle();});});
+$(document).ready(function(){$("#MAC2312").click(function(){$("#togMAC2312").toggle();});});
+$(document).ready(function(){$("#STA2122").click(function(){$("#togSTA2122").toggle();});});
+$(document).ready(function(){$("#STA2023").click(function(){$("#togSTA2023").toggle();});});
+
+// Scholarship in Practice -->
+$(document).ready(function(){$("#schol").click(function(){$("#togschol").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#ART2003Ctwo").click(function(){$("#togART2003Ctwo").toggle();});});
+$(document).ready(function(){$("#HUM2210two").click(function(){$("#togHUM2210two").toggle();});});
+$(document).ready(function(){$("#HUM2235two").click(function(){$("#togHUM2235two").toggle();});});
+$(document).ready(function(){$("#HUM2250two").click(function(){$("#togHUM2250two").toggle();});});
+$(document).ready(function(){$("#LDR2101").click(function(){$("#togLDR2101").toggle();});});
+$(document).ready(function(){$("#LDR3263").click(function(){$("#togLDR3263").toggle();});});
+$(document).ready(function(){$("#HUM3321three").click(function(){$("#togHUM3321three").toggle();});});
+$(document).ready(function(){$("#IFS2098").click(function(){$("#togIFS2098").toggle();});});
+$(document).ready(function(){$("#IFS2073four").click(function(){$("#togIFS2073four").toggle();});});
+$(document).ready(function(){$("#CLT3378three").click(function(){$("#togCLT3378three").toggle();});});
+
+// Social Sciences -->
+$(document).ready(function(){$("#socsci").click(function(){$("#togsocsci").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#LIS3103").click(function(){$("#togLIS3103").toggle();});});
+$(document).ready(function(){$("#ECO2000").click(function(){$("#togECO2000").toggle();});});
+$(document).ready(function(){$("#FAD2230").click(function(){$("#togFAD2230").toggle();});});
+$(document).ready(function(){$("#CCJ2020").click(function(){$("#togCCJ2020").toggle();});});
+$(document).ready(function(){$("#ECO2013").click(function(){$("#togECO2013").toggle();});});
+$(document).ready(function(){$("#ECO2023").click(function(){$("#togECO2023").toggle();});});
+$(document).ready(function(){$("#SYG2010two").click(function(){$("#togSYG2010two").toggle();});});
+$(document).ready(function(){$("#URS1006").click(function(){$("#togURS1006").toggle();});});
+$(document).ready(function(){$("#GEA1000two").click(function(){$("#togGEA1000two").toggle();});});
+$(document).ready(function(){$("#GEO1330").click(function(){$("#togGEO1330").toggle();});});
+$(document).ready(function(){$("#GEO1400two").click(function(){$("#togGEO1400two").toggle();});});
+$(document).ready(function(){$("#SYO3100").click(function(){$("#togSYO3100").toggle();});});
+$(document).ready(function(){$("#CCJ3011").click(function(){$("#togCCJ3011").toggle();});});
+$(document).ready(function(){$("#CCJ4662").click(function(){$("#togCCJ4662").toggle();});});
+$(document).ready(function(){$("#SYO3200two").click(function(){$("#togSYO3200two").toggle();});});
+$(document).ready(function(){$("#SYD3800two").click(function(){$("#togSYD3800two").toggle();});});
+
+// Oral Communication Competency -->
+$(document).ready(function(){$("#oralcomp").click(function(){$("#togoralcomp").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#SPC2608").click(function(){$("#togSPC2608").toggle();});});
+$(document).ready(function(){$("#COM2080").click(function(){$("#togCOM2080").toggle();});});
+
+// Upper-Division Writing Competency -->
+$(document).ready(function(){$("#writing").click(function(){$("#togwriting").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#MMC4300").click(function(){$("#togMMC4300").toggle();});});
+
+// Upper-Division Scholarship in Practice -->
+$(document).ready(function(){$("#upperschol").click(function(){$("#togupperschol").toggle();});});
+// Courses -->
+$(document).ready(function(){$("#STA3064").click(function(){$("#togSTA3064").toggle();});});
+$(document).ready(function(){$("#HIS4164").click(function(){$("#togHIS4164").toggle();});});
+
